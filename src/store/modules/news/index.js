e9f53192f1fcf236fbe5332b9d16ebe4883ab92d
Pierre Cavalet <pcavalet@kaliop.com>
2018-03-29 13:44:53 +0200

    init vuex store

---
+++ src/store/modules/news/index.js
@@ -0,0 +1,23 @@
+// initial state
+const state = {
+  lists: {
+    top: [],
+    new: [],
+    show: [],
+    ask: [],
+    job: []
+  }
+}
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
